f9a0fceb01494a51c88a8f5dc94e77bec315b167
Ritiek <ritiekmalhotra123@gmail.com>
2017-05-30 14:59:10 +0530

    Add slicing

---
+++ slicing/target/debug/deps/slicing-5dcfbf6d0af4a91b.d
@@ -0,0 +1,5 @@
+/home/pi/scripts/rust/challenges/CodeWars/Code-Practise/slicing/target/debug/deps/slicing-5dcfbf6d0af4a91b: src/main.rs
+
+/home/pi/scripts/rust/challenges/CodeWars/Code-Practise/slicing/target/debug/deps/slicing-5dcfbf6d0af4a91b.d: src/main.rs
+
+src/main.rs:
