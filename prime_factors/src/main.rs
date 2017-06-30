45080c39316980567742e96e3ff1d0f48ede176b
Ritiek <ritiekmalhotra123@gmail.com>
2017-06-30 19:19:20 +0530

    Add prime_factors

--- prime_factors/src/main.rs
+++ prime_factors/src/main.rs
@@ -1,11 +1,41 @@
-fn prime_factors(n: i64) -> String {
-	
-	
-	
-	"10".to_string()
+fn prime_factors(mut n: i64) -> String {
+    let mut result = String::new();
+    let mut count = 1;
+    let mut last_divider = 1;
+    while n != 1 {
+        for divider in 2..n+1 {
+            if n % divider == 0 {
+                n /= divider;
+
+                if divider == last_divider {
+                    count += 1;
+                } else if last_divider != 1 {
+                    if count == 1 {
+                        result = format!("{}({})", result, last_divider);
+                    } else {
+                        result = format!("{}({}**{})", result, last_divider, count);
+                        count = 1;
+                    }
+                }
+
+                if n == 1 {
+                    if count == 1 {
+                        result = format!("{}({})", result, divider);
+                    } else {
+                        result = format!("{}({}**{})", result, divider, count);
+                    }
+                } else {
+                    last_divider = divider;
+                    break;
+                }
+
+            }
+        }
+    }
+    result
 }
 
 fn main() {
-    let result = prime_factors(7775460);
+    let result = prime_factors(86240);
 	println!("{}", result);
 }
